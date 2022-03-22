1b73775c7f8efbfd0f01f07f75291b8b5d9bd703
Tomás Elizondo <tomaselizondo@piper.fibertel.com.ar>
2022-03-22 15:40:57 -0300

    cordobabitcoin

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,18 +5,17 @@ function Header() {
     <header className="flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-5">
         <Link href="/">
-          <img className="w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" alt="" />
+          <img className="w-44 object-contain cursor-pointer" src="/cordobabitcoin.png" alt="" />
         </Link>
         <div className="hidden md:inline-flex items-center space-x-5">
-          <h3>About</h3>
+          {/* <h3>About</h3>
           <h3>Contact</h3>
-          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full cursor-pointer">Follow</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full cursor-pointer">Follow</h3> */}
         </div>
       </div>
 
-      <div className="flex items-center space-x-5 text-green-600">
-        <h3>Sing In</h3>
-        <h3 className="border px-4 py-1 border-green-600 rounded-full">Get Started</h3>
+      <div className="flex items-center space-x-5 text-blue-300">
+        <h3 className="border px-4 py-1 border-blue-300 rounded-full"><a href="https://t.me/cordobabitcoin">telegram</a></h3>
       </div>
     </header>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,19 +13,20 @@ const Home = ({ posts }: Props) => {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Cordoba Bitcoin</title>
+        <link rel="icon" href="/bitcoin.png" />
       </Head>
 
       <Header />
 
-      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
-        <div className="px-10 space-y-5" >
-          <h1 className="text-6xl max-w-xl font-serif"><span className="underline decoration-black decoration-4">Medium</span> is a place to write, read and connect.</h1>
-          <h2>Its easy and free to post your thinking on any topic and connect with millon of readers.</h2>
+      <div className="bg-gray-200 border-y border-black py-10 lg:py-0">
+        <div className="flex justify-between items-center lg:mx-10">
+          <div className="px-10 space-y-5">
+            <h1 className="text-6xl max-w-xl font-serif"><span className="underline decoration-black decoration-4">Cordoba Bitcoin</span> es la comunidad cripto más grande del interior de Argentina.</h1>
+            <h2>Puedes unirte a nuestros eventos, debates y clases abiertas cuando quieras.</h2>
+          </div>
+          <img className="hidden md:inline-flex h-48 pr-10 lg:my-20" src="./bitcoin.png" alt="" />
         </div>
-        
-        <img className="hidden md:inline-flex h-48 lg:h-full" src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
       </div>
 
       {/* Posts */}
